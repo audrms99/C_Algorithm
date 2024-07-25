87f54b43b564a970c1994957de860a28f299421a
audrms99 <arm06201@gmail.com>
2024-07-26 04:36:56 +0900

    구현중

--- 04/ExpressionTree/ExpressionTree.h
+++ 04/ExpressionTree/ExpressionTree.h
@@ -7,22 +7,22 @@
 
 typedef char ElementType;
 
-typedef struct tagETNode 
+typedef struct tagETNode
 {
-    struct tagETNode* Left;
-    struct tagETNode* Right;
+    struct tagETNode *Left;
+    struct tagETNode *Right;
 
     ElementType Data;
 } ETNode;
 
-ETNode*   ET_CreateNode( ElementType NewData );
-void      ET_DestroyNode( ETNode* Node );
-void      ET_DestroyTree( ETNode* Root );
+ETNode *ET_CreateNode(ElementType NewData);
+void ET_DestroyNode(ETNode *Node);
+void ET_DestroyTree(ETNode *Root);
 
-void      ET_PreorderPrintTree( ETNode* Node );
-void      ET_InorderPrintTree( ETNode* Node );
-void      ET_PostorderPrintTree( ETNode* Node );
-void      ET_BuildExpressionTree( char* PostfixExpression, ETNode** Node );
-double    ET_Evaluate( ETNode* Tree );
+void ET_PreorderPrintTree(ETNode *Node);
+void ET_InorderPrintTree(ETNode *Node);
+void ET_PostorderPrintTree(ETNode *Node);
+void ET_BuildExpressionTree(char *PostfixExpression, ETNode **Node);
+double ET_Evaluate(ETNode *Tree);
 
 #endif
--- 04/ExpressionTree/pratice.c
+++ 04/ExpressionTree/pratice.c
@@ -9,7 +9,7 @@ typedef struct tagETNode
     struct tagETNode *Left;
     struct tagETNode *Right;
 
-    ElementType Data;
+    ElementType *Data;
 } ETNode;
 
 //------------------------------------------------------------------
@@ -20,7 +20,7 @@ ETNode *ET_CreateNode(ElementType NewData)
     NewNode->Left = NULL;
     NewNode->Right = NULL;
 
-    NewNode->Data = NewData;
+    NewNode->Data = &NewData;
 
     return NewNode;
 }
@@ -45,7 +45,7 @@ void ET_PreorderPrintTree(ETNode *Node)
     if (Node == NULL)
         return;
 
-    printf(" %c", Node->Data);
+    printf(" %s", Node->Data);
 
     ET_PreorderPrintTree(Node->Left);
     ET_PreorderPrintTree(Node->Right);
@@ -59,7 +59,7 @@ void ET_InorderPrintTree(ETNode *Node)
     printf("(");
     ET_InorderPrintTree(Node->Left);
 
-    printf("%c", Node->Data);
+    printf("%s", Node->Data);
 
     ET_InorderPrintTree(Node->Right);
     printf(")");
@@ -75,14 +75,31 @@ void ET_PostorderPrintTree(ETNode *Node)
     printf(" %c", Node->Data);
 }
 
-// 코드를 한번 생각해볼 필요가 있음
 void ET_BuildExpressionTree(char *PostfixExpression, ETNode **Node)
 {
+    // 두자리 이상의 숫자를 입력할 시 공백을 추가하여 구분해야할듯
+    // 토큰을 배열로 선언, 공백이 나오면 토큰에 저장하는걸 멈추기
+    // 사이사이 공백을 넣어야 할듯 ex) 10 2 * 5 2 - /
+    // 카운트도 필요할거 같음 개행문자 넣어줄 때 카운트
+    //
     int len = strlen(PostfixExpression);
-    char Token = PostfixExpression[len - 1];
-    PostfixExpression[len - 1] = '\0';
+    int cnt = 0;
+    char Token[3];
+    char lastStr = PostfixExpression[len - 1];
+    memset(Token, 0, sizeof(Token));
+    for (int i = 0; i < 3; i++)
+    {
+        if (PostfixExpression[len - 1 - i] == ' ')
+            break;
+        Token[i] = PostfixExpression[len - 1 - i];
+        cnt++;
+    }
+    for (int i = 0; i < cnt; i++)
+    {
+        PostfixExpression[len - 1 - i] = '\0';
+    }
 
-    switch (Token)
+    switch (lastStr)
     {
     case '+':
     case '-':
@@ -110,7 +127,7 @@ double ET_Evaluate(ETNode *Tree)
 
     if (Tree == NULL)
         return 0;
-    switch (Tree->Data)
+    switch (*Tree->Data)
     {
     case '+':
     case '-':
@@ -130,9 +147,9 @@ double ET_Evaluate(ETNode *Tree)
         break;
 
     default:
-        memset(Temp, 0, sizeof(Temp));
+        memset(Temp, 0, sizeof(Temp)); // Temp의 메모리를 초기화
         Temp[0] = Tree->Data;
-        Result = atof(Temp);
+        Result = atof(Temp); // string에 저장된 수를 숫자 값으로 변환
         break;
     }
 }
@@ -141,4 +158,26 @@ double ET_Evaluate(ETNode *Tree)
 
 int main(void)
 {
+    ETNode *Root = NULL;
+
+    char PostfixExpression[20] = "7 1 * 5 2 - /";
+    ET_BuildExpressionTree(PostfixExpression, &Root);
+
+    //  트리 출력
+    printf("Preorder ...\n");
+    ET_PreorderPrintTree(Root);
+    printf("\n\n");
+
+    printf("Inorder ... \n");
+    ET_InorderPrintTree(Root);
+    printf("\n\n");
+
+    printf("Postorder ... \n");
+    ET_PostorderPrintTree(Root);
+    printf("\n");
+
+    printf("Evaulation Result : %f \n", ET_Evaluate(Root));
+
+    //  트리 소멸시키기
+    ET_DestroyTree(Root);
 }
